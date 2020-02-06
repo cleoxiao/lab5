7e0760cb879f0a968c5d4d83f43b98ebd3538c79
cleoxiao <yax064@ucsd.edu>
2020-02-06 12:14:51 -0800

    change

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) {  
+	var inputName = request.query.name;
+	var inputDesc = request.query.description;
+	var newfriend = {
+	 'name': inputName,
+	 'description': inputDesc,
+	 'imageURL': 'http://lorempixel.com/400/400/people/',
+	};
+	data.friends.push(newfriend);
+	response.render('index', data);
+}	
